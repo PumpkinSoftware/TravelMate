265d8acb206cf1efb0df5fd2961708abecd2de7f
Andrea Asta <andreaasta@hotmail.it>
2019-01-09 23:48:14 +0100

    graphic login added

---
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.pumpkinsoftware.travelmate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button=(Button) findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain();
+            }
+        });
+    }
+
+    public void openMain(){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/MainActivity.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.pumpkinsoftware.travelmate;
+
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
