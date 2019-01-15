4de40854aff87bb3d1a046110e68dbc8b9fe8c6f
Romeo <romeo.bertoldo@gmail.com>
2019-01-15 16:17:59 +0100

    First api to add new travel with method get

--- Server/node/trip_api.js
+++ Server/node/trip_api.js
@@ -1,40 +1,51 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var Trip = require('./Schema_mongoose/trip_schema');
-var database = require('./database');
-//var url = process.env.DATA || "mongodb://127.0.0.1:27017/TravelMate";
+var TripSchema = require('./Schema_mongoose/trip_schema');
+//var database = require('./database');
+var url = process.env.DATA || "mongodb://127.0.0.1:27017/TravelMate";
 
 
+//Faccio collegare mongoose al database
+mongoose.connect(url);
+
+//Creo il router
 var router = express.Router(); 
 
 
+/******************************************
+			  Inizio API
+******************************************/
+
+/*****************************************/
+//Api per testare il funzionamento di trip_api.js
 
 router.get('/', function (req, res) {
     res.send("Mi trovo in trip_api.js");
 });
 
+
+/*****************************************/
+//Api per inserire un nuovo viaggio
+
 router.get('/new_trip', function(req,res){
-    const trip1 = new Trip({
-        name: 'First',
-        description: 'Sea',
-        partecipant: ['dbfhdsh@live.it', 'sjshhds@shdhs.it']
+
+	var toInsert = new TripSchema({
+		'name' : 'Road to Rome',
+        'description': 'Best travel to Rome',
+		'partecipant': [{'email':'roma@rome.it'},{'email':'nino@nino.it'}]
+	});
+
+	toInsert.save(function (err) {
+        if (err){ 
+        	res.send("Error to add"+toInsert.name+"on the database.");
+        	console.log("Error to add"+toInsert.name+"on the database.");
+        }
+        else {
+        	res.send("Trip " + toInsert.name + " created!");
+        	console.log("Trip " + toInsert.name + " created!");
+        }
     });
 
-    const trip2 = new Trip({
-        name: 'Second',
-        description: 'Desert',
-        partecipant: ['dbfhdsh@live.it', 'sjshhds@shdhs.it']
-    });
-
-    database.getDatabaseObject().collection('trip').insert(trip1);
-    database.getDatabaseObject().collection('trip').insert(trip2);
-
-    Trip.findOne({'name': 'Second'}, (err, trip) => {
-        if(err)
-            throw err;
-        console.log(trip);
-    });
 });
 
 module.exports = router;
-
