d8aae20e4c578e1a062db3e60bccc25fd550f3b0
Romeo <romeo.bertoldo@gmail.com>
2019-01-15 19:37:49 +0100

    new info about trip

--- Server/node/Schema_mongoose/trip_schema.js
+++ Server/node/Schema_mongoose/trip_schema.js
@@ -1,9 +1,13 @@
 var mongoose = require('mongoose');
 
 var TripSchema = new mongoose.Schema({
-        "name": String,
-        "description": String,
-		"partecipant": [{"email":String}]
+        name: String,
+        description: String,
+        departure: String,
+        destination: String,
+        budget: Number,
+        date: Date,
+		partecipant: [{email:String}]
 	}
 
 );
