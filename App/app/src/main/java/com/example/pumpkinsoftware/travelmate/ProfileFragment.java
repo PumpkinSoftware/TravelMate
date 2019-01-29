7d769050644b82b186d0f34f407139409ec594a1
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2019-01-29 13:47:30 +0100

    Add glide library (to be implemented...)

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/ProfileFragment.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/ProfileFragment.java
@@ -5,12 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.pumpkinsoftware.travelmate.glide.GlideApp;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        /*Loading images with glide */
+        /*ImageView img = getView().findViewById(R.id.profile);
+
+        GlideApp.with(getView().getContext())
+                .load(R.drawable.girl)
+                .into(img);
+
+        img = getView().findViewById(R.id.header_cover_image);
+
+        GlideApp.with(this)
+                .load(R.drawable.amsterdam)
+                .into(img);*/
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
