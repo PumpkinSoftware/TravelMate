1650a4ce662f98a14b993178f63013029b77e5fd
Romeo <romeo.bertoldo@gmail.com>
2019-02-03 00:51:06 +0100

    None

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/glide/MyAppGlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.example.pumpkinsoftware.travelmate.glide;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class MyAppGlideModule extends AppGlideModule {}
