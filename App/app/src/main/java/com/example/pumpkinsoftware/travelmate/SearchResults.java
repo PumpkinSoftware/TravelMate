089dffbc039f1d7a0628f7d284080031f1401478
Andrea Asta <andreaasta@hotmail.it>
2019-02-03 23:51:27 +0100

    prepared to show result

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/SearchResults.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/SearchResults.java
@@ -7,12 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SearchResults.OnFragmentInteractionListener} interface
+ * {@link SearchResults} interface
  * to handle interaction events.
  * Use the {@link SearchResults#newInstance} factory method to
  * create an instance of this fragment.
@@ -21,19 +23,22 @@ public class SearchResults extends Fragment {
     //template predefinito
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private TextView result;
+    private String stringaResult;
+
+    /*
+    serve se non si usa Bundle()
+    public void setArguments(String stringa) {
+        stringaResult=stringa;
+    }*/
+
 
     public SearchResults() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -43,11 +48,11 @@ public class SearchResults extends Fragment {
      * @return A new instance of fragment SearchResults.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchResults newInstance(String param1, String param2) {
+    //non valido
+    public static SearchResults newInstance(String stringaResult) {
         SearchResults fragment = new SearchResults();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(stringaResult,stringaResult);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +61,8 @@ public class SearchResults extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+          stringaResult = getArguments().getString("Key");
+           // mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -65,31 +70,26 @@ public class SearchResults extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_results, container, false);
+        View v= inflater.inflate(R.layout.fragment_search_results, container, false);
+
+        result=v.findViewById(R.id.result);
+        result.setText(getArguments().getString("Key"));
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
+      /*  if (mListener != null) {
             mListener.onFragmentInteraction(uri);
-        }
+        }*/
     }
 
-   /* @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
+
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
     /**
@@ -102,8 +102,6 @@ public class SearchResults extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
+
 }
