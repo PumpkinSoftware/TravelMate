14b31473a3f70e2baacc19e452ef30a1df30c6cf
666TheNumberOfTheBeast <xmetal1997@gmail.com>
2019-02-07 03:29:54 +0100

    Add Searchable activity & xml for searchview

---
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/SearchableActivity.java
@@ -0,0 +1,33 @@
+package com.example.pumpkinsoftware.travelmate;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search_results);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+            Toast.makeText(this, "Searching...", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            String message = intent.getStringExtra(MainActivity.FILTERED_QUERY);
+        }
+
+
+    }
+
+}
