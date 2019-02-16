9637dca36e43169b6b7441d1e59416e53acd778a
Andrea Asta <andreaasta@hotmail.it>
2019-02-16 15:10:25 +0100

    logout button

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/OpzioniFragment.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/OpzioniFragment.java
@@ -5,12 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class OpzioniFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_opzioni, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_opzioni, container, false);
+
+        Button b_logout = (Button) inflate.findViewById(R.id.button_logout);
+        b_logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return inflate;
     }
 }
