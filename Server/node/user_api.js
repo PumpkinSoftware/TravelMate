bfe50d5faa4869790b88b769c5bb8bb147dad592
Stef97 <sfuscon@live.it>
2019-02-22 16:11:08 +0100

    Inserted APIs for create new user, print all users and get user by email

--- Server/node/user_api.js
+++ Server/node/user_api.js
@@ -13,13 +13,109 @@ var router = express.Router();
 ******************************************/
 
 /*****************************************/
-
 //Api per testare il funzionamento di trip_api.js
 
 router.get('/', function (req, res) {
     res.send("Mi trovo in user_api.js");
 });
 
+/****************************************/
+//Api per inserire un nuovo utente
 
+router.post('/new_user', function(req, res){
+
+    var clientInput = req.body;
+
+    var conditions = {							
+		email: clientInput.email	
+    };
+    
+    UserSchema.findOne(conditions, function(err, user){
+        if (err){
+            
+            res.send(JSON.stringify({ status: "error", message: "Error with email" }));
+            console.log(err);
+        
+        }
+        else if (user){
+            
+            res.send(JSON.stringify({ status: "error", message: "User with this email is registered" }));
+            console.log(JSON.stringify({ status: "error", message: "User with this email is registered" }));
+        
+        }
+        else{
+            
+            var toInsert = new UserSchema({
+                name: clientInput.name.toLowerCase(),
+                surname: clientInput.surname.toLowerCase(),
+                nickname: clientInput.nickname.toLowerCase(),
+                age: clientInput.age,
+                gender: clientInput.gender.toLowerCase(),
+                relationship: clientInput.relationship.toLowerCase(),
+                email: clientInput.email,
+                description: clientInput.description.toLowerCase(),
+                sumReview: 0,
+                numReview: 0,
+                trips: [],
+                favouriteTrips: [],
+                comments: []
+            });
+            
+            toInsert.save(function (err) {
+                if (err){ 
+                    console.log(err);
+                    res.send(JSON.stringify({ status: "error", message: "Error to add "+toInsert.name+" on database." }));
+                    console.log(JSON.stringify({ status: "error", message: "Error to add "+toInsert.name+" on database." }));
+                }
+                else {
+                    res.send(JSON.stringify({ status: "success", message: "User " + toInsert.name + " created!" }));
+                    console.log(JSON.stringify({ status: "success", message: "User " + toInsert.name + " created!" }));
+                }
+            });
+        }
+    });
+});
+
+/****************************************/
+//Api per testing che stampa tutti gli utenti registrati
+
+router.get('/all_users', function(req, res){
+    
+    UserSchema.find({}, function(err, users){
+		if(err){
+			console.log(err);
+			res.send(JSON.stringify({ status: "error", message: "Error to send all users." }));
+		}else{
+			res.send(users);
+			console.log('retrieved list of users', users.length);
+		}
+	});
+});
+
+/****************************************/
+//Api per ottenere un utente tramite email => /getUserByEmail?email=prova@gmail.com
+
+router.get('/getUserByEmail', function(req, res){
+
+    var conditions = {
+        email: req.query.email
+    };
+
+    UserSchema.findOne(conditions, function(err, user){
+        
+        if (err){
+			res.send(JSON.stringify({ status: "error", message: "Error get with email" }));
+			console.log(err);
+		}
+		else if(user != null){
+			res.send(user);
+			console.log(user);
+        }
+        else{
+            res.send(JSON.stringify({ status: "error", message: "User not found" }));
+            console.log(JSON.stringify({ status: "error", message: "User not found" }));
+        }
+    });
+});
 
 module.exports = router;
