2bf17a1ca3272e3a70fa3ba5e520d603fb0d715a
Romeo <romeo.bertoldo@gmail.com>
2019-03-20 20:01:17 +0100

    Add chat

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/trips_adapter/TripsAdapterChat.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/trips_adapter/TripsAdapterChat.java
@@ -1,6 +1,7 @@
 package com.example.pumpkinsoftware.travelmate.trips_adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,7 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.pumpkinsoftware.travelmate.ChatActivity;
+import com.example.pumpkinsoftware.travelmate.CreationTrip;
 import com.example.pumpkinsoftware.travelmate.R;
+import com.example.pumpkinsoftware.travelmate.UserDetailsActivity;
+import com.example.pumpkinsoftware.travelmate.ViaggiFragment;
+import com.example.pumpkinsoftware.travelmate.chat.ChatActivityInside;
 import com.example.pumpkinsoftware.travelmate.glide.GlideApp;
 import com.example.pumpkinsoftware.travelmate.trip.Trip;
 
@@ -41,7 +47,7 @@ public class TripsAdapterChat extends RecyclerView.Adapter<TripsAdapterChat.View
                 @Override
                 public void onClick(View v) {
                     Trip t = getTrip();
-                    //apri chat
+                    apriChat(t);
                 }
             };
 
@@ -92,5 +98,12 @@ public class TripsAdapterChat extends RecyclerView.Adapter<TripsAdapterChat.View
         return trips.size();
     }
 
+
+    private void apriChat(Trip t){
+        Intent intent = new Intent(context, ChatActivityInside.class);
+        intent.putExtra(ChatActivityInside.EXTRA_TRIPID,t.getId() );
+        intent.putExtra(ChatActivityInside.EXTRA_TRIPNAME, t.getName());
+        context.startActivity(intent);
+    }
 
 }
