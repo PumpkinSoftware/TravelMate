1dfdefd7c580ce816739b3fd5062de9a5582d33c
Romeo <romeo.bertoldo@gmail.com>
2019-03-29 11:18:48 +0100

    Fix

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/FirebaseDynamicLinkHandler.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/FirebaseDynamicLinkHandler.java
@@ -25,7 +25,6 @@ public class FirebaseDynamicLinkHandler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_link);
-        System.out.println("I'm HERE");
         contesto = this;
 
         user = FirebaseAuth.getInstance().getCurrentUser();
