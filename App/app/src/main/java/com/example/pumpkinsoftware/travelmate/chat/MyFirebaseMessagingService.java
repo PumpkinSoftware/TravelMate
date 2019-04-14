d1f76209d4e135a75896f5e1ed7191d0b77e2949
Romeo <romeo.bertoldo@gmail.com>
2019-04-14 23:01:59 +0200

    Add code for notification

--- App/app/src/main/java/com/example/pumpkinsoftware/travelmate/chat/MyFirebaseMessagingService.java
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/chat/MyFirebaseMessagingService.java
@@ -15,21 +15,150 @@
  */
 package com.example.pumpkinsoftware.travelmate.chat;
 
-import android.util.Log;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.transition.Transition;
+import com.example.pumpkinsoftware.travelmate.R;
+import com.example.pumpkinsoftware.travelmate.TravelDetailsActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService{
+import java.util.Random;
 
-    private static final String TAG = "MyFMService";
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Handle data payload of FCM messages.
-        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
-        Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
-        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+
+        String type = remoteMessage.getData().get("type");
+
+        System.out.println("TYPE IS: "+type);
+
+        switch(type){
+            case "image": notificationImage(remoteMessage); break;
+            case "message" : notificationMessage(remoteMessage); break;
+            case "trip" : notificationTripInformation(remoteMessage); break;
+            default: notificationStandard(remoteMessage); break;
+        }
+
     }
 
+    public void notificationImage(RemoteMessage remoteMessage){
+
+        Intent notificationIntent = new Intent(this, TravelDetailsActivity.class);
+
+        //if(TravelDetailsActivity.isAppRunning && (TravelDetailsActivity.tripId.equals(remoteMessage.getData().get("tripId")))){
+        //    //Some action
+        //    return;
+        //}
+        /*else{
+            //Show notification as usual
+        }*/
+
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.default_trip);
+
+        Intent likeIntent = new Intent(this,TravelDetailsActivity.class);
+        likeIntent.putExtra(TravelDetailsActivity.EXTRA_ID,remoteMessage.getData().get("tripId"));
+        likeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0 /* Request code */, likeIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        /*
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels(notificationManager,"chatChannel");
+        }
+        */
+        RequestOptions myOptions = new RequestOptions().circleCrop();
+
+        /*MOLTO PROBABILMENTE L'ERRORE STA QUI ED E'QUESTO:
+        * java.lang.IllegalArgumentException: You must call this method on the main thread
+        * */
+        
+        Glide.with(this)
+                .asBitmap()
+                .load(remoteMessage.getData().get("image"))
+                .apply(myOptions)
+                .into(new MyTarget(this,remoteMessage,notificationManager,pendingIntent) {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+
+                        //You should use an actual ID instead
+                        int notificationId = new Random().nextInt(60000);
+
+                        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+                        NotificationCompat.Builder notificationBuilder =
+                                new NotificationCompat.Builder(this.getContext(), "chatChannel")
+                                        .setLargeIcon(resource)
+                                        .setSmallIcon(R.mipmap.ic_launcher)
+                                        .setContentTitle(this.getRemoteMessage().getData().get("title"))
+                                        .setStyle(new NotificationCompat.BigPictureStyle()
+                                                .setSummaryText(this.getRemoteMessage().getData().get("message"))
+                                                .bigPicture(resource))/*Notification with Image*/
+                                        .setContentText(this.getRemoteMessage().getData().get("message"))
+                                        .setAutoCancel(true)
+                                        .setSound(defaultSoundUri)
+                                        .setContentIntent(this.getPendingIntent());
+
+                        this.getNotificationManager().notify(notificationId, notificationBuilder.build());
+
+                    }
+                });
+
+    }
+
+    public void notificationMessage(RemoteMessage remoteMessage){
+        return;
+    }
+
+    public void notificationTripInformation(RemoteMessage remoteMessage){
+        return;
+    }
+
+    public void notificationStandard(RemoteMessage remoteMessage){
+        return;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(NotificationManager notificationManager, String channel){
+        CharSequence adminChannelName = getString(R.string.notifications_admin_channel_name);
+        String adminChannelDescription = getString(R.string.notifications_admin_channel_description);
+
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(channel, adminChannelName, NotificationManager.IMPORTANCE_LOW);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
+    }
 }
+
+/*
+* PER TESTARE: 
+* AVVIA L'APP, SUCCESSIVAMENTE IN VERBOSE SCRIVI 'TOKEN IS'
+* COPIA IL TOKEN ED ESEGUI https://debugtm.herokuapp.com/user/test?token=
+* AD APP APERTA SI VERIFICA IL CRASH
+* SE FUNZIONA, DOVRESTI VEDERE UNA NOTIFICA E CON LO SWIPE DOWN L'ANTPRIMA DI UN'IMMAGINE
+* */
---
+++ App/app/src/main/java/com/example/pumpkinsoftware/travelmate/chat/MyTarget.java
@@ -0,0 +1,52 @@
+package com.example.pumpkinsoftware.travelmate.chat;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyTarget extends SimpleTarget<Bitmap> implements Runnable{
+
+    private final Context context;
+    private final RemoteMessage remoteMessage;
+    private final NotificationManager notificationManager;
+    private final PendingIntent pendingIntent;
+
+    public MyTarget(Context context, RemoteMessage remoteMessage, NotificationManager notificationManager, PendingIntent pendingIntent) {
+        this.context = context;
+        this.remoteMessage = remoteMessage;
+        this.notificationManager = notificationManager;
+        this.pendingIntent = pendingIntent;
+    }
+
+    public PendingIntent getPendingIntent(){
+        return pendingIntent;
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    public RemoteMessage getRemoteMessage(){
+        return remoteMessage;
+    }
+
+    public NotificationManager getNotificationManager(){
+        return notificationManager;
+    }
+
+    @Override
+    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
